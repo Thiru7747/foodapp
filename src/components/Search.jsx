0ed3786733a4ca0164bcdfbeaef1b2917d6ce87f
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-11-10 23:54:19 +0530

    fetch api added

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "518badfb7cbd49f9b9fa7a3f81eba042";
+
 function Search() {
   const [query, setQuery] = useState("pizza");
 
   useEffect(() => {
-    function demo() {
-      console.log("useEffect called");
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
     }
-    demo();
+    fetchFood();
   }, [query]);
   return (
     <>
