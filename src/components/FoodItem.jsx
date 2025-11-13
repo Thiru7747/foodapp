3060fb518fa142f39fac62161b12ef3c35e84782
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2025-11-13 22:56:20 +0530

    add nav component

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,7 +1,9 @@
 function FoodItem({ food }) {
   return (
     <>
+      <img src={food.image} alt=""></img>
       <h1>{food.title}</h1>
+      <button>View Recipe</button>
     </>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,5 @@
+function Nav() {
+  return <div>Food App🍕</div>;
+}
+
+export default Nav;
